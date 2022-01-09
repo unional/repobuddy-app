064e0696cf5483b9ef05f501164e023f4961b6ef
Homa Wong <homawong@gmail.com>
2022-01-08 21:13:55 -0800

    update project

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  roots: ["<rootDir>/src/", "<rootDir>/test/"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$",
+  preset: 'ts-jest',
+  roots: ["<rootDir>/ts/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "node",
+  testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+  watchPlugins: [
+    'jest-watch-suspend',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    [
+      'jest-watch-toggle-config', { 'setting': 'verbose' },
+    ],
+    [
+      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
+    ]
+  ]
 };
--- ts/index.test.ts
+++ ts/index.test.ts
@@ -1,15 +1,13 @@
-// You can import your modules
-// import index from '../src/index'
-
 import nock from "nock";
-// Requiring our app implementation
-import myProbotApp from "../src";
 import { Probot, ProbotOctokit } from "probot";
-// Requiring our fixtures
+import fs from 'fs'
+import path from 'path'
+
+import myProbotApp from ".";
+
 import payload from "./fixtures/issues.opened.json";
+
 const issueCreatedBody = { body: "Thanks for opening this issue!" };
-const fs = require("fs");
-const path = require("path");
 
 const privateKey = fs.readFileSync(
   path.join(__dirname, "fixtures/mock-cert.pem"),
