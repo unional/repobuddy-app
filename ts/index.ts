90049110126f5e9d376483ea61422ceb7e0bb314
Homa Wong <homawong@gmail.com>
2022-01-09 00:04:20 -0800

    add event log

--- ts/index.ts
+++ ts/index.ts
@@ -1,18 +1,18 @@
 import { Probot } from 'probot'
 
 export = (app: Probot) => {
-  app.on('check_suite', (event)=> {
-    console.info('check_suite', event)
+  app.on('*', (event) => {
+    console.info(event.name, event.payload)
   })
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({
       body: 'Thanks for opening this issue!',
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
+    })
+    await context.octokit.issues.createComment(issueComment)
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-};
+}
