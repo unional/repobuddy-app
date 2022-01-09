4dc0e035adecb36a418901faf52823704982d110
Homa Wong <homawong@gmail.com>
2022-01-09 03:27:59 -0800

    chore: back to husky7
    somehow even husky4 is not working.
    
    Husky7 works now, although the message on vscode is not that clean

--- ts/checkSuiteRequested.ts
+++ ts/checkSuiteRequested.ts
@@ -2,7 +2,7 @@ import { Probot } from 'probot'
 
 export function checkSuiteRequested(app: Probot) {
   app.on('check_suite.requested', async (event) => {
-    // if ('.github' !== event.payload.repository.name) return
+    if ('.github' !== event.payload.repository.name) return
 
     const basehead = `${event.payload.check_suite.before!}...${event.payload.check_suite.after!}`
     await event.octokit.repos.compareCommitsWithBasehead({
